d8c2561ce85d113de7218856c5e11d3a27149219
Kiefer Jackson <kjacks34@asu.edu>
2023-05-25 19:58:59 -0700

    Created reusable modal component

---
+++ app/javascript/components/ModalMenu.js
@@ -0,0 +1,78 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ModalMenu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = Object.keys(this.props.formFields).reduce((fields, fieldName) => {
+			const { defaultValue } = this.props.formFields[fieldName];
+			fields[fieldName] = defaultValue;
+
+			return fields;
+		}, {});
+		this.getModal = () => document.getElementById(this.props.dialog_id);
+		this.handleFormChange = (event) => {
+			const { name, value } = event.target;
+			this.setState({ ...this.state, [name]: value });
+		}
+		this.handleClose = (e) => {
+			// Prevent the page from reloading upon button click
+			e.preventDefault();
+			
+			if (e.target.value === 'cancel') {
+				// Close the modal with no return value
+				this.getModal().close('');
+			}
+			else if (e.target.type === 'submit') {
+				// Get the modal's return value
+				console.log(this.getModal().returnValue);
+			}
+		}
+		this.handleSubmit = (e) => {
+			// Prevent the page from reloading upon button click
+			e.preventDefault();
+
+			// Get the modal element and close it, returning the form data contained in the state
+			this.getModal().close(e.target.value);
+		}
+		this.displayModal = (e) => {
+			// Prevent the page from reloading upon button click
+			e.preventDefault();
+			// Display modal menu
+			const modalEl = this.getModal();
+			modalEl.showModal();
+		}
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<button onClick={this.displayModal}>Show</button>
+				<dialog id={this.props.dialog_id}>
+					{Object.keys(this.props.formFields).map(fieldName => {
+						return (
+							<div className="input_block" key={this.props.dialog_id + fieldName}>
+								<label>
+									{fieldName}
+									<input { ...this.props.formFields[fieldName] } name={fieldName} value={this.state[fieldName]} onChange={this.handleFormChange} />
+								</label>
+							</div>
+						)
+					})}
+
+					<button onClick={this.handleClose} value="cancel">Cancel</button>
+					<button type="submit" onClick={this.handleSubmit} value={JSON.stringify(this.state)}>Submit</button>
+				</dialog>
+			</React.Fragment>
+		);
+	}
+	getReturnValue () {
+		console.log('The test function was called!!!');
+		return 'test';
+	}
+}
+
+ModalMenu.propTypes = {
+	dialog_id: PropTypes.string,
+	formFields: PropTypes.object,
+	handleFormChange: PropTypes.func
+};
+export default ModalMenu
--- app/javascript/components/dashboards/Main.js
+++ app/javascript/components/dashboards/Main.js
@@ -1,11 +1,20 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import ModalMenu from "../ModalMenu";
+
 class Main extends React.Component {
   render () {
     return (
       <React.Fragment>
         <div>
           THIS WOULD BE MAIN. NOT A WHOLE LOT RIGHT NOW.
+          <ModalMenu { ...{ 
+            dialog_id: 'test_modal',
+            formFields: {
+              'Text Field': { type: 'text', className: 'input_field string' }, 
+              'Number Field': { type: 'text', inputMode: 'decimal', pattern: '[0-9]', className: 'input_field float' }
+            }
+          } } />
         </div>
       </React.Fragment>
     );

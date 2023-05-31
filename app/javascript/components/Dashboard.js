409ffffe42e000f442764cb5b48cff25bf0b24b6
Kiefer Jackson <kjacks34@asu.edu>
2023-05-30 19:55:42 -0700

    Improved copolymer display for DefineSeries + test data model

--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -12,37 +12,76 @@ const copolymer_series_data = [
     name: 'PET Substitute',
     id: 1,
     description: "It's a substitute for PET.",
-    groupA: {
-      name: 'Diester',
-      copolymers: [
-        {
-          mass: 2,
-          wpercent: 40,
-          mpercent: 50,
-          moles: 0.02,
-          molar_mass: 100
-        },
-        {
-          mass: 3,
-          wpercent: 60,
-          mpercent: 50,
-          moles: 0.03,
-          molar_mass: 100
-        }
-      ]
-    },
-    groupB: {
-      name: 'Diol',
-      copolymers: [
-        {
-          mass: 4,
-          wpercent: 100,
-          mpercent: 100,
-          moles: 0.05,
-          molar_mass: 200
-        }
-      ]
-    }
+    groupA: 'Diester',
+    groupB: 'Diol',
+    copolymers: [
+      // Copolymer 1
+      {
+        total_mass: 9,
+        total_moles: 0.10,
+        groupA: [
+          {
+            name: 'CHDM',
+            mass: 4,
+            wpercent: 100,
+            mpercent: 100,
+            moles: 0.05,
+            molar_mass: 200
+          }
+        ],
+        groupB: [
+          {
+            name: 'DMOB',
+            mass: 2,
+            wpercent: 40,
+            mpercent: 50,
+            moles: 0.02,
+            molar_mass: 100
+          },
+          {
+            name: '1,2-something',
+            mass: 3,
+            wpercent: 60,
+            mpercent: 50,
+            moles: 0.03,
+            molar_mass: 100
+          }
+        ]
+      },
+      // Copolymer 2
+      {
+        total_mass: 15,
+        total_moles: 0.125,
+        groupA: [
+          {
+            name: 'CHDM',
+            mass: 5,
+            wpercent: 100,
+            mpercent: 100,
+            moles: 0.025,
+            molar_mass: 200
+          }
+        ],
+        groupB: [
+          {
+            name: 'DMOB',
+            mass: 3.33,
+            wpercent: 33.3,
+            mpercent: 10,
+            moles: 0.0333,
+            molar_mass: 100
+          },
+          {
+            name: '1,2-something',
+            mass: 6.66,
+            wpercent: 66.6,
+            mpercent: 90,
+            moles: 0.0666,
+            molar_mass: 100
+          }
+        ]
+      }
+    ]
   }
 ]
 
@@ -64,7 +103,12 @@ class Dashboard extends React.Component {
           this.setState({ ...this.state, dashboard: CREATE_SERIES });
           break;
         case DEFINE_SERIES:
-        this.setState({ ...this.state, dashboard: DEFINE_SERIES, selectedSeries: copolymer_series_data.find(series => series.name === data) });
+          this.setState({ 
+              ...this.state, 
+              dashboard: DEFINE_SERIES, 
+              // `data` in this context is the given name for the copolymer series
+              selectedSeries: copolymer_series_data.find(series => series.name === data) 
+          });
           break;
         default:
           this.setState({ ...this.state, dashboard: '[UNKNOWN ACTION]' });
